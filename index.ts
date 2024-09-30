86e9d7a371097fcb0485e045073dc0841ed77be4
Dan Cryptoscan <dancryptoscan@gmail.com>
2024-09-30 17:26:03 +0700

    feat: initialize new TypeScript project with FileMap class and configuration files
    chore: add .gitignore to exclude node_modules and build artifacts
    
    build: create package.json for project metadata and dependencies
    
    build: add tsconfig.json for TypeScript compiler options and settings

---
+++ index.ts
@@ -0,0 +1,34 @@
+﻿import { writeFileSync, readFileSync, existsSync } from "node:fs";
+
+export default class FileMap extends Map<string, string> {
+  public constructor(
+    name: string,
+    entries?: ReadonlyArray<[string, string]> | null,
+  ) {
+    if (!existsSync(name)) {
+      writeFileSync(name, "{}");
+    }
+    if (existsSync(name)) {
+      const data: Record<string, string> = JSON.parse(
+        readFileSync(name).toString(),
+      );
+      super(Object.entries(data) as ReadonlyArray<[string, string]>);
+      super.set("fileName", name);
+      return;
+    }
+
+    super(entries);
+    super.set("fileName", name as string);
+  }
+
+  public set(key: string, value: string): this {
+    super.set(key, value);
+
+    writeFileSync(
+      super.get("fileName") as string,
+      JSON.stringify(Object.fromEntries(super.entries()), undefined, 2),
+    );
+
+    return this;
+  }
+}
